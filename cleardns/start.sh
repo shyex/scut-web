ee0d42af47c9a7af18797899a67729e8481d73de
dnomd343 <i@343.re>
2022-03-11 00:07:07 +0800

    update: cleardns version 1.2

--- cleardns/start.sh
+++ cleardns/start.sh
@@ -1,14 +1,19 @@
-temp=`docker ps -a | grep -o cleardns`
-if [ "$temp" != "" ]; then
-  docker rm -f cleardns > /dev/null
+CERT_DIR="/etc/ssl/certs/scut.343.re"
+if [ "$(docker ps -a | grep -o cleardns)" != "" ]; then
+  docker restart cleardns > /dev/null
+else
+  docker run -d \
+  --privileged \
+  --name cleardns \
+  --restart always \
+  --network macvlan \
+  --hostname cleardns \
+  --volume $CERT_DIR:$CERT_DIR \
+  --volume /usr/bin/frpc:/usr/bin/frpc \
+  --volume /etc/cleardns:/etc/cleardns \
+  --volume /etc/timezone:/etc/timezone:ro \
+  --volume /etc/localtime:/etc/localtime:ro \
+  dnomd343/cleardns:v1.2 > /dev/null
 fi
-docker run --restart always \
---name cleardns \
---network macvlan \
---privileged -d \
--v /etc/cleardns:/etc/cleardns \
--v /etc/timezone:/etc/timezone:ro \
--v /etc/localtime:/etc/localtime:ro \
-dnomd343/cleardns:latest > /dev/null
 sleep 1s
 docker ps -a
