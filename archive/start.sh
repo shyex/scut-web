19041823ae4a9f8bc5192aa4469fb716e423d05a
dnomd343 <i@343.re>
2022-03-10 23:48:23 +0800

    update: new archive docker

--- archive/start.sh
+++ archive/start.sh
@@ -1,14 +1,15 @@
-temp=`docker ps -a | grep -o archive`
-if [ "$temp" != "" ]; then
-    docker rm -f archive > /dev/null
+if [ "$(docker ps -a | grep -o archive)" != "" ]; then
+  docker rm -f archive > /dev/null
 fi
-docker run --restart always -d -it \
---name archive \
--v /etc/archive/:/etc/archive/ \
--v /etc/scutweb/log/:/etc/scutweb/log/ \
--v /etc/route/log/:/etc/route/log/ \
--v /etc/timezone:/etc/timezone:ro \
--v /etc/localtime:/etc/localtime:ro \
-archive:latest > /dev/null
+docker run -dit \
+  --name archive \
+  --restart always \
+  --hostname archive \
+  --volume /etc/archive:/etc/archive \
+  --volume /etc/route/log:/etc/route/log \
+  --volume /etc/scutweb/log:/etc/scutweb/log \
+  --volume /etc/timezone:/etc/timezone:ro \
+  --volume /etc/localtime:/etc/localtime:ro \
+  archive:latest > /dev/null
 sleep 1s
 docker ps -a
