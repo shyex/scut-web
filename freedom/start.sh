4a2470d92d0f56e61fc3ddf78b65b25d78229fc8
dnomd343 <i@343.re>
2022-03-11 00:16:14 +0800

    update: script of freedom web

--- freedom/start.sh
+++ freedom/start.sh
@@ -1,19 +1,20 @@
-temp=`docker ps -a | grep -o freedom`
-if [ "$temp" != "" ]; then
-    docker rm -f freedom > /dev/null
+if [ "$(docker ps -a | grep -o freedom)" != "" ]; then
+  docker rm -f freedom > /dev/null
 fi
-docker run --restart always \
---name freedom \
---privileged -d \
---network macvlan \
---ip=192.168.2.5 \
---dns=192.168.2.4 \
--e V2RAYA_ADDRESS=0.0.0.0:80 \
--e V2RAYA_V2RAY_BIN=/usr/bin/xray \
--v /etc/freedom:/etc/v2raya \
--v /usr/bin/xray:/usr/bin/xray \
--v /etc/timezone:/etc/timezone:ro \
--v /etc/localtime:/etc/localtime:ro \
-mzz2017/v2raya > /dev/null
+docker run -d \
+  --privileged \
+  --name freedom \
+  --restart always \
+  --hostname freedom \
+  --network macvlan \
+  --ip 192.168.2.5 \
+  --dns 192.168.2.4 \
+  --env V2RAYA_V2RAY_BIN=xray \
+  --env V2RAYA_ADDRESS=0.0.0.0:80 \
+  --volume /etc/freedom:/etc/v2raya \
+  --volume /usr/bin/xray:/usr/bin/xray \
+  --volume /etc/timezone:/etc/timezone:ro \
+  --volume /etc/localtime:/etc/localtime:ro \
+  mzz2017/v2raya > /dev/null
 sleep 1s
 docker ps -a
